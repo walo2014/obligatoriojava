f646bbc2500dc42143a0e4037550cc80fcfad7e5
lmachado <lmachado@LuisMachado-PC.farmashop.com.uy>
2017-06-19 20:45:01 -0300

    sc

--- ObligatorioCarné/src/Persona.java
+++ ObligatorioCarné/src/Persona.java
@@ -1,4 +1,14 @@
+import java.security.Timestamp;
 
 public class Persona {
 
+	private String nombre;
+	private String apellido;
+	private Timestamp fechaNacimiento;
+	private String lugarNacimiento;
+	private String cedula;
+	private char genero;
+	
+	
+	
 }
