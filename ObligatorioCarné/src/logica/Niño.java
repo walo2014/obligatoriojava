35e0bb5def5a964c55256c604747cc551c8335f1
Walo <Walo@Walo-PC>
2017-07-02 12:21:20 -0300

    contructor

--- ObligatorioCarné/src/logica/Niño.java
+++ ObligatorioCarné/src/logica/Niño.java
@@ -3,14 +3,28 @@ import java.util.Calendar;
 
 public class Niño {
 	//parametros
-private String nombre;
-private int documento;
-private Calendar fechaNacimiento;
-private String servicioMedico;
-private String medicoCabecera;
-private boolean tieneFonasa;
-private Registro registros;
-private int cantRegistrosIngresados;
+	private String nombre;
+	private int documento;
+	private Calendar fechaNacimiento;
+	private String servicioMedico;
+	private String medicoCabecera;
+	private boolean tieneFonasa;
+	private Registro registros;
+	private int cantRegistrosIngresados;
+
+	public Niño(String nom,int doc,Calendar fecNac,String serMed,String medCab,boolean tieFon,Registro reg,int canRegIng)
+		{
+			nombre = nom;
+			documento = doc;
+			fechaNacimiento = fecNac;
+			servicioMedico = serMed;
+			medicoCabecera = medCab;
+			tieneFonasa = tieFon;
+			registros = reg;
+			cantRegistrosIngresados = canRegIng;
+		
+		
+		}
 	
 	
 
