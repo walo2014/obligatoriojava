0db58764e209a2b489093732aae4a3608dd0933e
Walo <Walo@Walo-PC>
2017-07-02 12:07:10 -0300

    f

--- ObligatorioCarné/src/logica/Niño.java
+++ ObligatorioCarné/src/logica/Niño.java
@@ -1,6 +1,15 @@
 package logica;
 
-public class Niño {
+public class Niño () {
 	//Pendiente de programar por Luis
-
+	//parametros
+		private String nombre;
+		private int documento;
+		private Calendar fechaNacimiento;
+		private String servicioMedico;
+		private String medicoCabecera;
+		private boolean tieneFonasa;
+		private Registro registros;
+		private int cantRegistrosIngresados;
+		
 }
