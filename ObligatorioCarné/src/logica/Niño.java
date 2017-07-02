03f39344afc716e92b51f19f3ab199235d4679eb
Walo <Walo@Walo-PC>
2017-07-02 12:23:04 -0300

    a

--- ObligatorioCarné/src/logica/Niño.java
+++ ObligatorioCarné/src/logica/Niño.java
@@ -24,8 +24,71 @@ public class Niño {
 			registros = reg;
 			cantRegistrosIngresados = canRegIng;
 		
-		
 		}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(int documento) {
+		this.documento = documento;
+	}
+
+	public Calendar getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Calendar fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getServicioMedico() {
+		return servicioMedico;
+	}
+
+	public void setServicioMedico(String servicioMedico) {
+		this.servicioMedico = servicioMedico;
+	}
+
+	public String getMedicoCabecera() {
+		return medicoCabecera;
+	}
+
+	public void setMedicoCabecera(String medicoCabecera) {
+		this.medicoCabecera = medicoCabecera;
+	}
+
+	public boolean isTieneFonasa() {
+		return tieneFonasa;
+	}
+
+	public void setTieneFonasa(boolean tieneFonasa) {
+		this.tieneFonasa = tieneFonasa;
+	}
+
+	public Registro getRegistros() {
+		return registros;
+	}
+
+	public void setRegistros(Registro registros) {
+		this.registros = registros;
+	}
+
+	public int getCantRegistrosIngresados() {
+		return cantRegistrosIngresados;
+	}
+
+	public void setCantRegistrosIngresados(int cantRegistrosIngresados) {
+		this.cantRegistrosIngresados = cantRegistrosIngresados;
+	}
 	
 	
 
