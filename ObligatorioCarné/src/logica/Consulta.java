339cec6689dcc208087868f5c9e62e6ec1a22295
Maria <Maria@DESKTOP-QMFAVER>
2017-07-03 01:13:06 -0300

    ultima modificacion Clase Registro /Clase Consulta 03/07 01:11hrs Maria

--- ObligatorioCarné/src/logica/Consulta.java
+++ ObligatorioCarné/src/logica/Consulta.java
@@ -1,6 +1,37 @@
 package logica;
-
-public class Consulta {
-	//Pendiente de programar por Maria
-	
+//Pendiente de programar por Maria
+public class Consulta extends Registro{
+	//subclase de la clase Registro
+	private String medico;
+	private String diagnostico;
+	private String recomendaciones;
+	//constructor
+	public Consulta(int fecha, String comentario, String medico, String diagnostico,String recomendaciones){
+		super (fecha, comentario);
+		this.medico=medico;
+		this.diagnostico=diagnostico;
+		this.recomendaciones=recomendaciones;
+	}
+	public String getMedico() {
+		return medico;
+	}
+	public void setMedico(String medico) {
+		this.medico = medico;
+	}
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+	public String getRecomendaciones() {
+		return recomendaciones;
+	}
+	public void setRecomendaciones(String recomendaciones) {
+		this.recomendaciones = recomendaciones;
+	}
 }
+
+
+
+	
--- ObligatorioCarné/src/logica/Registro.java
+++ ObligatorioCarné/src/logica/Registro.java
@@ -1,6 +1,31 @@
 package logica;
 import java.util.Calendar;
-
+//Pendiente de programar
 public class Registro {
-	//Pendiente de programar
+	private int fecha;//verificar si es int??
+	private String comentario;// debe ir en mayuscula es mas apropiado??
+	//constructor
+	public Registro(int fecha, String comentario){
+		this.fecha = fecha;
+		this.comentario = comentario;
+
+	}
+	public int getFecha() {
+		return fecha;
+	}
+	public void setFecha(int fecha) {
+		this.fecha = fecha;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+}
+
+		
+		
+	}
+	
 }
