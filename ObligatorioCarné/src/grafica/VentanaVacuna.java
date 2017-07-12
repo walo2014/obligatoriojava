8a42308b44428acfd18ddb1967c3900be540e098
Maria <Maria@DESKTOP-QMFAVER>
2017-07-12 14:25:49 -0300

    vvacuna

--- ObligatorioCarné/src/grafica/VentanaVacuna.java
+++ ObligatorioCarné/src/grafica/VentanaVacuna.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class VentanaVacuna extends JFrame {
 
@@ -35,8 +36,12 @@ public class VentanaVacuna extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(52, 97, 46, 14);
+		contentPane.add(lblNewLabel);
 	}
 
 }
