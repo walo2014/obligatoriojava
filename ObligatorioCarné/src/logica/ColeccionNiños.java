a4bbb53caeb399ff80d055204bad01836d3877ad
Walo <Walo@Walo-PC>
2017-07-15 17:28:28 -0300

    a

--- ObligatorioCarné/src/logica/ColeccionNiños.java
+++ ObligatorioCarné/src/logica/ColeccionNiños.java
@@ -4,14 +4,12 @@ import javax.swing.JOptionPane;
 
 public class ColeccionNiños {
 	private Niño[] arreglo;
-
 	private int topeNiños;
 	private int maxNiños;
 	private int maxRegistro;
 	public ColeccionNiños() {
 
 		arreglo = null;
-		// arreglo = new Niño[2];
 		topeNiños = 0;
 		maxNiños = 0;
 		maxRegistro=0;
@@ -25,36 +23,22 @@ public class ColeccionNiños {
 
 	}
 
-	//Profe: no deberia haber sido boolean
+
 	public boolean agregar(Niño n) {
 
-		// Se tiene que mostrar en la interfaz grafica la pantalla
+
 		if (topeNiños == arreglo.length) {
 			return false;
-			// JOptionPane.showMessageDialog(null, "Ya no puede ingresar mas
-			// niños al sistema","ERROR" ,JOptionPane.ERROR_MESSAGE);
+
 		} else {
 			arreglo[topeNiños] = n;
-			//System.out.println("AGREGADO EL NIÑO " + n.getNombre() + " en la posicion " + topeNiños);
-			//JOptionPane.showMessageDialog(null, "El niño ha sido ingresado al sistema correctamente");
+
 			topeNiños++;
 			return true;
 		}
 	}
 	
-	public String[] listarNiños(){
-		
-				{
-					String[]  listaNiños = new String[topeNiños] ;
-					for(int i = 0; i< topeNiños; i++)
-					{
-						listaNiños[i] = arreglo[i].getNombre();
-						System.out.println(listaNiños[i] + " posicion "+i);
-					}
-					return listaNiños;
-		}
-		
-	}
+
 
 	public int getMaxNiños() {
 		return maxNiños;
