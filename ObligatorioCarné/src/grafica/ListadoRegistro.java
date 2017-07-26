b347d2a007c72848c7fad9822763ffe8d0bc9727
OEM <OEM@DESKTOP-KJVMMU7>
2017-07-26 16:21:00 -0300

    mnk

---
+++ ObligatorioCarné/src/grafica/ListadoRegistro.java
@@ -0,0 +1,89 @@
+package grafica;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import logica.ColeccionNiños;
+import logica.Niño;
+
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.AbstractListModel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+
+public class ListadoRegistro extends JFrame {
+
+	private JPanel contentPane;
+
+
+
+	private ColeccionNiños coleccion;
+	private JTextField txtCedula;
+	private JList lstVacunas;
+	
+	public ListadoRegistro(ColeccionNiños coleccion) {
+		setBounds(100, 100, 450, 468);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtCedula = new JTextField();
+		txtCedula.setBounds(128, 37, 86, 20);
+		contentPane.add(txtCedula);
+		txtCedula.setColumns(10);
+		
+		JLabel lblCedula = new JLabel("Cedula");
+		lblCedula.setBounds(52, 40, 46, 14);
+		contentPane.add(lblCedula);
+		
+		JButton btnBuscar = new JButton("buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscarNiño();
+			}
+		});
+		btnBuscar.setBounds(230, 36, 89, 23);
+		contentPane.add(btnBuscar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(158, 192, 2, 2);
+		contentPane.add(scrollPane);
+		
+		lstVacunas = new JList();
+		lstVacunas.setBounds(38, 75, 370, 343);
+		contentPane.add(lstVacunas);
+		lstVacunas.setModel(new AbstractListModel() {
+			String[] values = new String[] {""};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		this.coleccion=coleccion;
+	}
+
+	protected void buscarNiño() {
+		int cedula=Integer.parseInt(txtCedula.getText());
+		Niño niño=coleccion.getDatosNiño(cedula);
+		if(niño==null){
+			JOptionPane.showMessageDialog(null, "El niño no esta registrado");
+			
+		}
+		else{
+			lstVacunas.setListData(niño.listadoRegistro());
+		}
+		
+	}
+}
---
+++ ObligatorioCarné/src/grafica/VentanaContadorConsulta.java
@@ -0,0 +1,125 @@
+package grafica;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import excepciones.ExisteNinioException;
+import logica.ColeccionNiños;
+import logica.Control;
+import logica.Niño;
+import logica.Registro;
+import logica.RegistroVacuna;
+import logica.Vacuna;
+
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Date;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.UIManager;
+
+public class VentanaContadorConsulta extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnAplicar;
+	private ColeccionNiños coleccion;
+	private Niño niño;
+	private JComboBox cbxDia;
+	private JComboBox cbxMes;
+	private JComboBox cbxAño;
+	private JLabel lblCantidad;
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaContadorConsulta(ColeccionNiños col) {
+		setTitle("Consultas M\u00E9dicas");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 221);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		coleccion=col;
+		
+		JLabel lblComentarios = new JLabel("Cantidad Total :");
+		lblComentarios.setBounds(10, 101, 106, 14);
+		contentPane.add(lblComentarios);
+		
+		btnAplicar = new JButton("Calcular");
+		btnAplicar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//cargo variable del formulario
+				
+				int dia=cbxDia.getSelectedIndex()+1;
+				int mes=cbxMes.getSelectedIndex();
+				int anio=Integer.parseInt(cbxAño.getSelectedItem().toString());
+
+				Calendar calendario=Calendar.getInstance();
+				calendario.set(anio, mes, dia);
+				
+				int cantidad=coleccion.cantidadConsultasAntesFecha(calendario);
+				lblCantidad.setText(""+cantidad);
+			
+			}});
+		btnAplicar.setBounds(160, 136, 89, 23);
+		contentPane.add(btnAplicar);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(UIManager.getColor("InternalFrame.activeTitleBackground"));
+		panel.setBounds(10, 21, 414, 49);
+		contentPane.add(panel);
+		
+		cbxDia = new JComboBox();
+		cbxDia.setBounds(36, 11, 60, 20);
+		panel.add(cbxDia);
+		
+		JLabel label = new JLabel("Dia");
+		label.setBounds(10, 14, 46, 14);
+		panel.add(label);
+		
+		JLabel label_1 = new JLabel("Mes");
+		label_1.setBounds(133, 14, 46, 14);
+		panel.add(label_1);
+		
+		cbxMes = new JComboBox(new String[] {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"});
+		cbxMes.setBounds(169, 11, 66, 20);
+		panel.add(cbxMes);
+		
+		JLabel label_2 = new JLabel("A\u00F1o");
+		label_2.setBounds(276, 14, 29, 14);
+		panel.add(label_2);
+		
+		cbxAño = new JComboBox();
+		cbxAño.setBounds(305, 11, 82, 20);
+		panel.add(cbxAño);
+		
+		lblCantidad = new JLabel("");
+		lblCantidad.setBounds(126, 101, 46, 14);
+		contentPane.add(lblCantidad);
+		
+		for(int dia=1;dia<=31;dia++)
+		{
+			cbxDia.addItem(dia);
+		}
+		int anio=(new Date().getYear()+1900)-15;
+		for(int a=anio;a<=anio+15;a++)
+		{
+			cbxAño.addItem(a);
+		}
+		
+	}
+}
